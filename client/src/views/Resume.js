5ed307f9f8db1f0f1e641613e5b9817b49eeb61c
Kacy Gilbert <gilbertkacy90@gmail.com>
2023-04-18 10:04:28 -0500

    began building resume page

---
+++ client/src/views/Resume.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import aboutPic from '../images/aboutPic.jpeg'
+import { Helmet } from 'react-helmet'
+
+const Resume = () => {
+  return (
+    <div>Resume</div>
+  )
+}
+
+export default Resume
