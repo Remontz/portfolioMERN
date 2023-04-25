cba720c70824708a7d07e6dc714c4dbf8efcab5a
Kacy Gilbert <gilbertkacy90@gmail.com>
2023-04-25 16:19:58 -0500

    erased useEffect import

--- client/src/views/Resume.js
+++ client/src/views/Resume.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import {jsPDF} from 'jspdf';
 import html2canvas from 'html2canvas';
 import { Helmet } from 'react-helmet'
